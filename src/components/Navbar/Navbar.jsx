cdbffe94fe6b807d9e874d0c12b5d039cb138b52
Kushal Pradhan <kushalpradhan127@gmail.com>
2026-02-17 00:10:32 +0530

    Navbar completed withour responsive

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect } from "react";
+import { FiMenu } from "react-icons/fi";
+import { FaGithub, FaLinkedin, FaLinkedinIn } from "react-icons/fa";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [activeSection, setActiveSection] = useState();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //scroll function
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  };
+
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "experience", label: "Experience" },
+    { id: "contact", label: "Contact" },
+    { id: "work", label: "Work" },
+  ];
+
+  return (
+    <div
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="text-white py-5 flex justify-between items-center">
+        {/* Logo */}
+        <div
+          className=" cursor-pointer
+        
+        text-lg font-semibold justify-between items-center "
+        >
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Kushal</span>
+          <span className="text-[#8245ec]">/</span>
+          <span className="text-white">Pradhan</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className="hidden  md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={` cursor-pointer hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }`}
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        {/* Social Media Icons */}
+        <div className="hidden md:flex space-x-6">
+          <a
+            href="https://github.com/Kushalcreation"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kushal-pradhan-491061394/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#8245ec]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
