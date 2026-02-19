76fce33dd23b01e23a907fc1e40984eb92eda37c
KUSHAL PRADHAN <kushalpradhan127@gmail.com>
2026-02-19 15:56:29 +0530

    Navbar completed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import ReactTypingEffect from "react-typing-effect";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/*Left Side Content*/}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/*Greeting*/}
+          <h1 className="text-3xl sm:text-5xl md:text-4xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          <h2 className="text-4xl sm:text-5xl md:text-5xl font-bold text-white mb-4 leading-tight">
+            Kushal Pradhan
+          </h2>
+          {/* */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a</span>
+            <ReactTypingEffect>
+              text={["Fullstack developer", "App Developer", "Coder"]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRender=
+              {(cursor) => <span className="text-[#8245ec]">{cursor}</span>}
+            </ReactTypingEffect>
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
