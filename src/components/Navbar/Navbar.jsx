d6c317c495aabda8953866ecc414a79027105f1c
NAVEEN DAS <107771166+dasnaveen25@users.noreply.github.com>
2026-02-19 16:02:07 +0530

    navbar update this

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
           : "bg-transparent"
       }`}
     >
-      <div className="text-white py-7 flex justify-between items-center">
+      <div className="text-white py-8 flex justify-between items-center">
         {/* Logo */}
         <div
           className=" cursor-pointer
@@ -70,7 +70,7 @@ const Navbar = () => {
         </ul>
 
         {/* Social Media Icons */}
-        <div className="hidden md:flex space-x-6">
+        <div className="hidden md:flex space-x-4">
           <a
             href="https://github.com/Kushalcreation"
             target="_blank"
@@ -88,7 +88,58 @@ const Navbar = () => {
             <FaLinkedin size={24} />
           </a>
         </div>
+
+        {/* Mobile Screen Icon*/}
+
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#8245ec] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
+        </div>
       </div>
+      {/* Mobile Menu Items */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 -translate-x-1/2 w-4/5 bg-[#050414]/50 backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+          <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className={`cursor-pointer hover:text-white ${activeSection === item.id ? "text-[#8245ec]" : ""}`}
+              >
+                <button onClick={() => handleMenuItemClick(item.id)}>
+                  {item.label}
+                </button>
+              </li>
+            ))}
+            <div className="flex space-x-4">
+              <a
+                href="https://github.com/Kushalcreation"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/kushal-pradhan-491061394/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaLinkedin size={24} />
+              </a>
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
